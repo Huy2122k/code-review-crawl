1c26fceda5ec848ba0c16a649f08fb8956d0c3c8
huy2122k <wwwhuy212@gmail.com>
2024-10-02 00:57:23 +0700

    Initial commit

---
+++ get.sh
@@ -0,0 +1,6 @@
+gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /repos/sourcery-ai/sourcery/pulls/290/reviews/1144164550/comments
+
+
+https://github.com/hacksider/Deep-Live-Cam/pull/88
+
+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
